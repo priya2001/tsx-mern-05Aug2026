64870c48deaa0a249b409cf8e4b47beea27e0c8a
Priya <priya.2023CA75@mnnit.ac.in>
2026-08-05 20:40:32 +0530

    picsum image ids modified

--- src/features/characters/utils/characterMedia.ts
+++ src/features/characters/utils/characterMedia.ts
@@ -1,6 +1,6 @@
 import type { Character } from '../../../types/swapi';
 
-const PICSUM_IMAGE_IDS: readonly number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 237,30,100,101];
+const PICSUM_IMAGE_IDS: readonly number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 237,30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200];
 
 const hashString = (value: string): number => {
   let hash = 0;
